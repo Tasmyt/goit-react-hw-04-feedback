45d3bde55acec9f9cff7ab468c83272c39f75f1b
Tasmyt <121162710+Tasmyt@users.noreply.github.com>
2023-07-22 09:16:59 +0300

    start

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -36,6 +36,6 @@ Statistics.propTypes = {
     good: PropTypes.number.isRequired,
     neutral: PropTypes.number.isRequired,
     bad: PropTypes.number.isRequired,  
-  total: PropTypes.func.isRequired,
+    total: PropTypes.func.isRequired,
   positiveFeedbackPercentage: PropTypes.func.isRequired,
 };
